fcb26894f4347bd70709b5afa1892643379098b6
Eve96-M <evemele45@gmail.com>
2022-09-28 14:06:57 -0430

    fourth commit

--- src/Components/NavBar.jsx
+++ src/Components/NavBar.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
-import '../NavBar.css'
+import '../NavBar.css';
+import Logo from '../assets/Logo.png';
 const NavBar = () => {
     return (
         <header>
             <nav className='navBar'>
                 <div className='navAccent'>
-                    <img src="" alt="" className='logo' />
+                    <img src={Logo} alt="" className='logo' />
                 </div>
                 <div className='btn-Wrapper'>
                     <button className='nav-Btn'></button>
